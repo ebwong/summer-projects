e9b37bf9a2bc4de31d0d63a356c6fd87833c62b6
Andrew Le <a1le@ucsd.edu>
2019-07-10 13:15:01 -0700

    Switched from pytest to unittest; simple XML report generation

--- docker/python-docker-demo/test_fibonacci_pytest.py
+++ docker/python-docker-demo/test_fibonacci_pytest.py
@@ -3,18 +3,28 @@ Tests for Fibonacci number function
 """
 from fibonacci import compute_fib
 import pytest
+import unittest
+import xmlrunner
 
-def test_compute_small_fib():
-    assert compute_fib(7) == 13
-    print("Test 'test_compute_small_fib' complete")
+class TestFibonaaci(unittest.TestCase):
 
-def test_compute_large_fib():
-    assert compute_fib(20) == 6765
-    print("Test 'test_compute_large_fib' complete")
+    def test_compute_small_fib(self):
+        self.assertEqual(compute_fib(7), 13)
+
+    def test_compute_large_fib(self):
+        self.assertEqual(compute_fib(20), 6765)
+
+    def test_invalid_int_arg(self):
+        with self.assertRaises(ValueError):
+            compute_fib(-1)
+
+if __name__ == '__main__':
+    unittest.main(
+        testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
+        # these make sure that some options that are not applicable
+        # remain hidden from the help menu
+        failfast=False, buffer=False, catchbreak=False
+    )
 
 
-def test_invalid_int_arg():
-    with pytest.raises(ValueError):
-        compute_fib(-1)
-        print("Test 'test_invalid_int_arg' complete")
 
