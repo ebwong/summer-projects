3713348498712dd64eedbb006c33d43bc0bdc0a7
Andrew Le <a1le@ucsd.edu>
2019-07-31 17:00:59 -0700

    Added some methods to get Business data

--- get_census_data.py
+++ get_census_data.py
@@ -2,6 +2,7 @@
 Queries 2014 ASE data
 """
 import requests
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -20,29 +21,30 @@ def get_census_var_string(census_vars):
     delimiter = ","
     return delimiter.join(census_vars)
 
-def query_census(url, census_vars):
+def query_census(url, census_vars, api_key=None):
     """
     Returns census data as a list of JSON objects
     :param url: the URL containing the census data
     :param census_vars: a tuple of census variables to query for
-    :return: census data in as a list of JSON objects
+    :param api_key: An API key for using the Census API; defaults to None
+    :return: census data  as a list of JSON objects
     """
 
     census_var_string = get_census_var_string(census_vars)
     params = {
         "get": census_var_string,
-        "for": "us:*" # in the entire U.S.
+        "for": "us:*", # in the entire U.S.
+        "key": api_key
     }
     response = requests.get(url, params=params)
-    # print(response.status_code)
-    # print(response.content)
     return response.json()
 
-def format_census_data(census_data):
+def dataframe_from_census(census_data, columns_to_drop=None):
     """
-    Formats the census data as a Pandas Dataframe
+    Formats the census data as a Pandas DataFrame
     :param census_data: the given census data
-    :return: census data as a Pandas Dataframe
+    :param columns_to_drop: columns to omit from the returned DataFrame
+    :return: census data as a Pandas DataFrame
     """
 
     census_data_as_list_of_dicts = []
@@ -53,46 +55,151 @@ def format_census_data(census_data):
         # to list
         census_data_as_list_of_dicts.append(pd.Series(
             dict(zip(census_data_labels, census_data[i]))))
-    return pd.DataFrame(census_data_as_list_of_dicts)
 
-def format_census_data_for_graphing(census_data):
-    data = []
-    pass
+    df_census = pd.DataFrame(census_data_as_list_of_dicts)
+    # Get rid of some unnecessary columns
+    return df_census.drop(columns=columns_to_drop)
 
-def plot_data(data):
-    x_spacing = np.arange(len(data[1]))
-    plt.bar(x_spacing, data[1])
-    plt.xlabel("Education")
-    plt.ylabel("Number of business owners")
 
-    plt.xticks(x_spacing, data[0], fontsize=5, rotation=30)
 
-    plt.title("Education vs number of business owners")
+def format_owner_data_for_graphing(census_data):
+    x_var = [elem for elem in census_data["YIBSZFI_TTL"]]
+    y_var = [int(elem, 10) for elem in census_data["OWNPDEMP"]]
+    return [x_var, y_var]
+
+
+def plot_owner_data(data):
+    x_var_index = 0
+    y_var_index = 1
+    x_label = "Years in business"
+    y_label = "Number of business owners"
+    x_spacing = np.arange(len(data[x_var_index]))
+    plt.bar(x_spacing, data[y_var_index])
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+
+    plt.xticks(x_spacing, data[y_var_index], fontsize=5, rotation=30)
+
+    plt.title(f"{x_label} vs {y_label}")
     plt.show()
 
-def get_census_data():
-    url = "https://api.census.gov/data/2014/ase/cscbo"
-    census_vars = ("OWNPDEMP", "HRSWRKD", "HRSWRKD_TTL", "YIBSZFI",
-                   "YIBSZFI_TTL", "USBORNCIT", "USBORNCIT_TTL")  # The variables to query for
-    return query_census(url, census_vars)
 
-data = format_census_data(get_census_data())
-filtered_data = data.query(" HRSWRKD == 'CZ' ")
-# print(filtered_data["YIBSZFI_TTL"])
-# print(data.index.values)
+def get_owner_data():
+    """
+    Returns Census ASE Business Owner data
+    :return:
+    """
+
+    owner_url = "https://api.census.gov/data/2014/ase/cscbo"
+    owner_vars = ("ASECBO", "ASECBO_TTL", "OWNPDEMP", "USBORNCIT")  # The variables to query for
+    api_key = "5fff67c7b4559d14fc4adc2da294f674e95c1a3e"
+    return query_census(owner_url, owner_vars, api_key)
+
+def get_business_data():
+    """
+    Returns Census ASE Business data
+    :return:
+    """
+
+    business_url = "https://api.census.gov/data/2014/ase/cscb"
+    business_vars = ("ASECB", "ASECB_TTL", "EMP", "PAYANN", "YIBSZFI")
+    api_key = "5fff67c7b4559d14fc4adc2da294f674e95c1a3e"
+
+    return query_census(business_url, business_vars, api_key)
+
+def simplify_business_df(df_bus):
+    # Formatting business data
+    df_bus = df_bus.rename(columns={"ASECB": "RACE"})
+    # Make codes uniform
+    # A mapping of the codes to replace
+    bus_codes_to_replace = {
+        "0000": "00",
+        "0001": "01",
+        "0002": "02",
+        "0010": "20",
+        "0030": "29",
+        "0100": "30",
+        "0200": "40",
+        "0300": "50",
+        "0400": "60",
+        "0500": "70",
+        "0600": "80",
+        "0700": "90",
+        "0900": "92",
+        "1000": "V2",
+        "3000": "V4",
+    }
+
+    # Rename codes that should be kept
+    df_bus = df_bus.replace(bus_codes_to_replace)
+
+    # Remove extraneous rows
+    bus_codes_to_remove = [
+        "0003", "0020", "0800", "2000", "9996", "9998"
+    ]
+    df_bus = df_bus[np.logical_not(df_bus["RACE"].isin(bus_codes_to_remove))]
+
+    # Make sure numeric data are treated as numbers
+    df_bus = df_bus.astype({"EMP": np.int64, "PAYANN": np.int64})
+
+    return df_bus
+
+def add_columns_to_business_df(df_bus):
+    pay_per_emp = df_bus["PAYANN"] // df_bus["EMP"]
+
+
+    df_bus = df_bus.assign(PAYPEREMP= pay_per_emp)
+    return df_bus
+
+def main():
+    owner = False
+    if owner:
+        owner_data = get_owner_data()
+        owner_cols = ["us", "USBORNCIT"]
+        df_own = dataframe_from_census(owner_data, owner_cols)
+        df_own = df_own.rename(columns={"ASECBO": "RACE"})
+        # Collapse rows with same race code
+
+
+        print(df_own)
+
+    bus = True
+    if bus:
+        bus_data = get_business_data()
+        bus_cols = ["us"]
+        df_bus = dataframe_from_census(bus_data, bus_cols)
+        df_bus = simplify_business_df(df_bus)
+        df_bus = add_columns_to_business_df(df_bus)
+
+
+        print(df_bus)
+
+        # print(df_bus.to_csv())
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
 
 
 
 
 run_tests = False
 if run_tests:
-    data = format_census_data(get_census_data())
-    print(data)
-    plot_data(data)
+    data = dataframe_from_census(get_owner_data())
+    filtered_data = data.query(" HRSWRKD == 'CZ' ")
+    plot_owner_data(format_owner_data_for_graphing(filtered_data))
 
 output_to_file = False
 if output_to_file:
-    output = format_census_data(get_census_data())
+    output = dataframe_from_census(get_owner_data())
     file = open(".\output.txt", mode="w+")
     file.write(str(output))
     file.close()
